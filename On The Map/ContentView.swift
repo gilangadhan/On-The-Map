f3a1a3ba2a4bce30947d5500505eebf234ee29f2
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-06-05 18:21:56 +0700

    Add Project

---
+++ On The Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  On The Map
+//
+//  Created by Gilang Ramadhan on 05/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ On The Map/On_The_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_The_MapApp.swift
+//  On The Map
+//
+//  Created by Gilang Ramadhan on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct On_The_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
